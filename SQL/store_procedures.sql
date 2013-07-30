e648d740f37d51e41037f085c98b014a45fb43ba
leggettr <richard.leggett@tgac.ac.uk>
2013-07-30 11:08:46 +0100

    Corrected spelling errors.

--- SQL/store_procedures.sql
+++ SQL/store_procedures.sql
@@ -90,10 +90,9 @@ CREATE OR REPLACE VIEW property
 AS
 SELECT
     analysis_property.analysis_id as id, 
-	IF(property="tool", value, NULL) AS tool  ,
     IF(property="Encoding", value, NULL) AS encoding  ,
     IF(property="cassava_version", value, NULL) AS casava  ,
-    IF(property="chmestry_version", value, NULL) AS chemestry  ,
+    IF(property="chemistry_version", value, NULL) AS chemistry  ,
     IF(property="instrument", value, NULL) AS instrument  ,
     IF(property="software_on_instrument_version", value, NULL) AS software  ,
     IF(property="type_of_experiment", value, NULL) AS type  ,
@@ -112,7 +111,6 @@ CREATE OR REPLACE VIEW run
 AS
 SELECT 
     property.id as analysis_id, 
-	MAX(property.tool) as tool,
     MAX(property.encoding) as encoding, 
     MAX(property.casava) as casava,  
     MAX(property.chemestry) as chemestry,
@@ -127,15 +125,6 @@ SELECT
 FROM property AS property
 GROUP BY id $$
 
-
-CREATE OR REPLACE view latest_run
-AS 
-SELECT 
-	MAX(analysis_id) as analysis_id,  tool, encoding, casava, chemestry, instrument, software, type, pair, sample_name, lane, run, barcode 
-FROM
-	run
-GROUP BY   tool, encoding, casava, chemestry, instrument, software, type, pair, sample_name, lane, run, barcode $$
-
 DROP PROCEDURE IF EXISTS general_summaries_for_run$$
 
 CREATE PROCEDURE 	general_summaries_for_run(
@@ -145,12 +134,8 @@ CREATE PROCEDURE 	general_summaries_for_run(
 	IN pair_in VARCHAR(500),
 	IN barcode_in VARCHAR(500))
 BEGIN	
-	SELECT description as Description,
-		AVG(value) as Average, 
-		COUNT(*) as Samples,
-		sum(value) as Total 
-	FROM 
-		value_type, analysis_value, latest_run as run, type_scope
+	SELECT description, AVG(analysis_value.value) as Average FROM 
+		value_type, analysis_value, run, type_scope
 	WHERE
 		type_scope.scope = "analysis"
 		AND type_scope.id=value_type.type_scope_id 
@@ -165,7 +150,7 @@ BEGIN
 
 END$$
 
-call general_summaries_for_run(NULL, NULL, NULL, NULL, NULL)$$
+-- call general_summaries_for_run(NULL, NULL, NULL, NULL, NULL)$$
 
 -- call general_summaries_for_run(NULL, NULL, "1", NULL, NULL)$$
 
@@ -184,10 +169,8 @@ BEGIN
     SELECT  
     position as Position, 
     size as Size, 
-    AVG(value) as Average, 
-	COUNT(*) as Samples,
-	sum(value) as Total
-    FROM value_type, per_partition_value,type_scope, latest_run as run
+    AVG(per_partition_value.value) as Average
+    FROM run, value_type, per_partition_value,type_scope   
     WHERE type_scope.scope = "base_partition"
 		AND type_scope.id=value_type.type_scope_id  
 		AND value_type.description = partition_value
@@ -198,81 +181,11 @@ BEGIN
 		AND IF(lane_in  IS NULL, TRUE,  run.lane = lane_in ) 
 		AND IF(pair_in IS NULL, TRUE, run.pair = pair_in)
 		AND IF(barcode_in IS NULL, TRUE, run.barcode = barcode_in)
-    GROUP BY value_type.id, position, size
-	ORDER BY Position;
+    GROUP BY value_type.id, position, size;
 END$$
- call summary_per_position_for_run("quality_mean", NULL, NULL, NULL, NULL, NULL)$$
+-- call summary_per_position_for_run("quality_mean", NULL, NULL, NULL, NULL, NULL)$$
 
 -- call summary_per_position_for_run("quality_mean",NULL, NULL, "1", NULL, NULL)$$
 
 -- call summary_per_position_for_run("quality_mean",NULL, NULL, "2", NULL, NULL)$$
-
-DROP PROCEDURE IF EXISTS summary_value_with_comment$$
-CREATE PROCEDURE summary_value_with_comment(
-IN scope_in VARCHAR(45), 
-IN instrument_in VARCHAR(500),
-IN run_in VARCHAR(500),
-IN lane_in VARCHAR(500),
-IN pair_in VARCHAR(500),
-IN barcode_in VARCHAR(500))	
-BEGIN
-	SELECT  
-	description as Description,
-	comment as Comment, 
-	AVG(value) as Average, 
-	COUNT(*) as Samples,
-	sum(value) as Total
-	FROM type_scope, value_type, analysis_value, latest_run as run 
-	WHERE scope=scope_in 
-		AND type_scope.id =value_type.type_scope_id 
-		AND analysis_value.value_type_id = value_type.id 
-		AND analysis_value.analysis_id=run.analysis_id
-		AND IF(instrument_in IS NULL, TRUE, run.instrument = instrument_in)
-		AND IF(run_in IS NULL, TRUE, run.run = run_in)
-		AND IF(lane_in  IS NULL, TRUE,  run.lane = lane_in ) 
-		AND IF(pair_in IS NULL, TRUE, run.pair = pair_in)
-		AND IF(barcode_in IS NULL, TRUE, run.barcode = barcode_in)		
-	GROUP BY 
-		description, comment
-	ORDER BY 
-		Total DESC 
-		;
-	
-END$$
-
-call summary_value_with_comment("overrepresented_sequence", NULL, NULL, NULL, NULL, NULL)$$
-
-DROP PROCEDURE IF EXISTS summary_value$$
-CREATE PROCEDURE summary_value(
-IN scope_in VARCHAR(45), 
-IN instrument_in VARCHAR(500),
-IN run_in VARCHAR(500),
-IN lane_in VARCHAR(500),
-IN pair_in VARCHAR(500),
-IN barcode_in VARCHAR(500))	
-BEGIN
-	SELECT  
-	description as Description,
-	AVG(value) as Average, 
-	COUNT(*) as Samples,
-	sum(value) as Total
-	FROM type_scope, value_type, analysis_value, latest_run as run 
-	WHERE scope=scope_in 
-		AND type_scope.id =value_type.type_scope_id 
-		AND analysis_value.value_type_id = value_type.id 
-		AND analysis_value.analysis_id=run.analysis_id
-		AND IF(instrument_in IS NULL, TRUE, run.instrument = instrument_in)
-		AND IF(run_in IS NULL, TRUE, run.run = run_in)
-		AND IF(lane_in  IS NULL, TRUE,  run.lane = lane_in ) 
-		AND IF(pair_in IS NULL, TRUE, run.pair = pair_in)
-		AND IF(barcode_in IS NULL, TRUE, run.barcode = barcode_in)	
-	GROUP BY 
-		description, comment
-	ORDER BY
-		Total Desc
-		;
-END$$
-
-call summary_value("multiplex_tag", NULL, NULL, NULL, NULL, NULL)$$
-
 delimiter ;
