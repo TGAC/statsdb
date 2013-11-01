c10a7d6bac0ae4969ca6f594dabc43bf72a6675f
Robert Davey <robert.davey@tgac.ac.uk>
2013-11-01 14:39:06 +0000

    Final functions added

--- Perl/Reports.pm
+++ Perl/Reports.pm
@@ -45,7 +45,14 @@ sub list_global_analyses() {
 }
 
 sub list_per_base_summary_analyses() {
+  my $self = shift;
+  my $con = $self->get_connection();
 
+  my $statement = "CALL list_summary_per_scope(?)";
+  my $sth = $con->prepare($statement) || die $con->errstr;
+  $sth->execute("base_partition");
+
+  return Reports::ReportTable->new($sth);
 }
 
 sub get_per_position_summary() {
@@ -63,7 +70,17 @@ sub get_per_position_summary() {
 }
 
 sub get_average_value() {
+  my $self = shift;
+  my $analysis = shift;
+  my $analysis_property = shift;
+  my $analysis_property_value = shift;
 
+  my $con = $self->get_connection();
+  my $statement = "CALL general_summary(?,?,?)";
+  my $sth = $con->prepare($statement) || die $con->errstr;
+  $sth->execute($analysis, $analysis_property, $analysis_property_value);
+
+  return Reports::ReportTable->new($sth);
 }
 
 sub get_average_values() {
@@ -92,27 +109,110 @@ sub get_average_values() {
 }
 
 sub get_per_position_values() {
+  my $self = shift;
+  my $analysis = shift;
+  my %properties = shift;
 
+  my @args = (undef, undef, undef, undef, undef, undef);
+  $args[0] = $analysis;
+  $args[1] = $properties{INSTRUMENT} if exists $properties{INSTRUMENT};
+  $args[2] = $properties{RUN} if exists $properties{RUN};
+  $args[3] = $properties{LANE} if exists $properties{LANE};
+  $args[4] = $properties{PAIR} if exists $properties{PAIR};
+  $args[5] = $properties{BARCODE} if exists $properties{BARCODE};
+
+  my $statement = "CALL summary_per_position_for_run(?,?,?,?,?,?)";
+  my $con = $self->get_connection();
+  my $sth = $con->prepare($statement) || die $con->errstr;
+  $sth->bind_param(1, $args[0]);
+  $sth->bind_param(2, $args[1]);
+  $sth->bind_param(3, $args[2]);
+  $sth->bind_param(4, $args[3]);
+  $sth->bind_param(5, $args[4]);
+  $sth->bind_param(6, $args[5]);
+
+  $sth->execute();
+
+  return Reports::ReportTable->new($sth);
 }
 
 sub get_summary_values_with_comments() {
+  my $self = shift;
+  my $scope = shift;
+  my %properties = shift;
 
+  my @args = (undef, undef, undef, undef, undef, undef);
+  $args[0] = $scope;
+  $args[1] = $properties{INSTRUMENT} if exists $properties{INSTRUMENT};
+  $args[2] = $properties{RUN} if exists $properties{RUN};
+  $args[3] = $properties{LANE} if exists $properties{LANE};
+  $args[4] = $properties{PAIR} if exists $properties{PAIR};
+  $args[5] = $properties{BARCODE} if exists $properties{BARCODE};
+
+  my $statement = "CALL summary_value_with_comment(?,?,?,?,?,?)";
+  my $con = $self->get_connection();
+  my $sth = $con->prepare($statement) || die $con->errstr;
+  $sth->bind_param(1, $args[0]);
+  $sth->bind_param(2, $args[1]);
+  $sth->bind_param(3, $args[2]);
+  $sth->bind_param(4, $args[3]);
+  $sth->bind_param(5, $args[4]);
+  $sth->bind_param(6, $args[5]);
+
+  $sth->execute();
+
+  return Reports::ReportTable->new($sth);
 }
 
 sub get_summary_values() {
+  my $self = shift;
+  my $scope = shift;
+  my %properties = shift;
 
-}
+  my @args = (undef, undef, undef, undef, undef, undef);
+  $args[0] = $scope;
+  $args[1] = $properties{INSTRUMENT} if exists $properties{INSTRUMENT};
+  $args[2] = $properties{RUN} if exists $properties{RUN};
+  $args[3] = $properties{LANE} if exists $properties{LANE};
+  $args[4] = $properties{PAIR} if exists $properties{PAIR};
+  $args[5] = $properties{BARCODE} if exists $properties{BARCODE};
 
-sub get_result_table_from_stored_procedure() {
+  my $statement = "CALL summary_value(?,?,?,?,?,?)";
+  my $con = $self->get_connection();
+  my $sth = $con->prepare($statement) || die $con->errstr;
+  $sth->bind_param(1, $args[0]);
+  $sth->bind_param(2, $args[1]);
+  $sth->bind_param(3, $args[2]);
+  $sth->bind_param(4, $args[3]);
+  $sth->bind_param(5, $args[4]);
+  $sth->bind_param(6, $args[5]);
 
+  $sth->execute();
+
+  return Reports::ReportTable->new($sth);
 }
 
 sub get_analysis_properties() {
+  my $self = shift;
+  my $con = $self->get_connection();
 
+  my $statement = "CALL list_selectable_properties()";
+  my $sth = $con->prepare($statement) || die $con->errstr;
+  $sth->execute();
+
+  return Reports::ReportTable->new($sth);
 }
 
 sub get_values_for_property() {
+  my $self = shift;
+  my $property = shift;
+  my $con = $self->get_connection();
 
+  my $statement = "CALL list_selectable_values_from_property(?)";
+  my $sth = $con->prepare($statement) || die $con->errstr;
+  $sth->execute($property);
+
+  return Reports::ReportTable->new($sth);
 }
 
 sub list_all_runs_for_instrument() {
@@ -139,19 +239,42 @@ sub list_all_runs {
 }
 
 sub list_lanes_for_run() {
+  my $self = shift;
+  my $run = shift;
 
+  my $con = $self->get_connection();
+  my $statement = "SELECT lane FROM run WHERE `run` = ? GROUP BY lane";
+  my $sth = $con->prepare($statement) || die $con->errstr;
+  $sth->execute($run);
+
+  return Reports::ReportTable->new($sth);
 }
 
 sub list_barcodes_for_run_and_lane() {
+  my $self = shift;
+  my $run = shift;
+  my $lane = shift;
 
+  my $con = $self->get_connection();
+  my $statement = "SELECT barcode FROM run WHERE `run` = ? AND `lane` = ? GROUP BY barcode";
+  my $sth = $con->prepare($statement) || die $con->errstr;
+  $sth->execute($run, $lane);
+
+  return Reports::ReportTable->new($sth);
 }
 
 sub get_samples_from_run_lane_barcode() {
+  my $self = shift;
+  my $run = shift;
+  my $lane = shift;
+  my $barcode = shift;
 
-}
+  my $con = $self->get_connection();
+  my $statement = "SELECT sample_name FROM run WHERE `run` = ? AND `lane` = ? AND `barcode` = ?";
+  my $sth = $con->prepare($statement) || die $con->errstr;
+  $sth->execute($run, $lane, $barcode);
 
-sub result_set_to_array() {
-
+  return Reports::ReportTable->new($sth);
 }
 
 1;
